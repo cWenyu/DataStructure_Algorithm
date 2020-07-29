60f83173302771d96aa5d6934b446bd624ca640f
Wenyu Shi <d00198805@student.dkit.ie>
2020-07-29 20:40:32 +0100

    选择排序

--- src/sort/SelectSort.java
+++ src/sort/SelectSort.java
@@ -2,15 +2,15 @@ package sort;
 
 import java.util.Arrays;
 
-public class QuickSort {
+public class SelectSort {
     public static void main(String[] args) {
 //        int[] arr = {1,2,3,4,5};
         int[] arr = {32, 534, 876, 43, 9};
-        quickSort(arr);
+        selectSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void quickSort(int[] arr) {
+    public static void selectSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int min = arr[i];
             int minIndex = i;
