e1d56c39585184a40016b4fadf23230fa5bd1327
Wenyu Shi <d00198805@student.dkit.ie>
2020-08-03 16:41:09 +0100

    希尔排序

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -12,8 +12,8 @@ public class BubbleSort {
 
     public static void bubbleSort(int[] arr) {
         boolean swap = false;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     int temp = arr[j];
                     arr[j] = arr[i];
